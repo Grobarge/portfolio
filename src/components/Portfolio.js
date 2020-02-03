0567a57a7191e536477134e8c0912bd9cc2a15be
Gaje Robarge <gajer@paulmitchell.edu>
2020-02-03 15:35:44 -0700

    updated projects with jamstack store

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import '../App.css';
-
+import React, { Component } from "react";
+import "../App.css";
 
 export default class Porfolio extends Component {
   render() {
@@ -8,28 +7,43 @@ export default class Porfolio extends Component {
       <section id="portfolio">
         <h1>Projects</h1>
         <div className="cards">
-
-          <a className="card" id="dropurspot" href="https://github.com/Grobarge/dropurspot">
-          </a>
-          <a className="card" id="bidmycrib" href="https://www.bidmycrib.com">
-          </a>
-          <a className="card" id="todoapp" href="https://github.com/Grobarge/react-native-todo">
-          </a>
-          <a className="card" id="houser" href="https://github.com/Gaje-Robarge/houser">
-          </a>
-          <a className="card" id="shelfie" href="https://github.com/Gaje-Robarge/shelfie">
-          </a>
-          <a className="card" id="tableapp" href="https://simpletableapp.netlify.com">
-
-          </a>
-
-
+          <a
+            className="card"
+            id="dropurspot"
+            href="https://github.com/Grobarge/dropurspot"
+          ></a>
+          <a
+            className="card"
+            id="bidmycrib"
+            href="https://www.bidmycrib.com"
+          ></a>
+          <a
+            className="card"
+            id="todoapp"
+            href="https://github.com/Grobarge/react-native-todo"
+          ></a>
+          <a
+            className="card"
+            id="jamstack"
+            href="https://jamstackstore.netlify.com/"
+          ></a>
+          <a
+            className="card"
+            id="tableapp"
+            href="https://simpletableapp.netlify.com"
+          ></a>
+          <a
+            className="card"
+            id="houser"
+            href="https://github.com/Gaje-Robarge/houser"
+          ></a>
+          <a
+            className="card"
+            id="shelfie"
+            href="https://github.com/Gaje-Robarge/shelfie"
+          ></a>
         </div>
-
-
       </section>
     );
   }
 }
-
-
